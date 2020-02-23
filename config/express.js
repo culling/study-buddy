0237f1b131b8d498d7b4c1a7ffec85d90763ddd8
Gene Culling <culling66@gmail.com>
2020-02-23 14:44:20 +1300

    added HTTPS

--- config/express.js
+++ config/express.js
@@ -1,15 +1,36 @@
-var config  = require('./config');
-var http    = require('http');
+const fs = require('fs');
+var config = require('./config');
+
+
+var http = require('http');
+var https = require('https');
+var privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 var express = require('express');
 
-module.exports = function(){
-    var app     = express();
-    var server  = http.createServer(app);
 
-   var index = require("./../server/routes/index.route.server");
-   app.use("/", index);
+module.exports = function () {
+
+    // Express
+    let app = express();
+    //Http Server
+    let server = http.createServer(app);
+    //Https Server
+    let credentials = { key: privateKey, cert: certificate };
+    let httpsServer = https.createServer(credentials, app);
+
+
+    var index = require("./../server/routes/index.route.server");
+    app.use("/", index);
 
     //static files
     app.use(express.static('./public'));
-    return server;
+
+    let serverConfig = {
+        http: server,
+        https: httpsServer
+    }
+
+    //return server;
+    return serverConfig;
 }
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const express   = require('./config/express');
-const app       = express(); 
+const serverConfig   = require('./config/express');
 
+//console.log("serverConfig: ", serverConfig());
+
+const httpApp   = serverConfig().http; 
 const port      = 3000;
-app.listen(port, () => console.log(`Listening on ${port}`));
-module.exports = app;
+httpApp.listen(port, () => console.log(`Listening on ${port}`));
+
+const httpsApp   = serverConfig().https; 
+const httpsPort      = 3001;
+httpsApp.listen(httpsPort, () => console.log(`Listening on ${httpsPort}`));
+
+
+module.exports = httpApp;
