6849499b82faf04d1b6e81a1bc9049d97d3fb2ff
Gene Culling <culling66@gmail.com>
2021-02-25 06:58:06 +1300

    Archive old project

--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // "globDirectory": "public/",
-  // "globPatterns": [
-    // "**/*.{json,js,md,yml,sh,css,lock,html,nuspec,ps1,scss,svg,png,xml,ico,txt,jsx,mp3}"
-  // ],
-  // "swDest": "public/sw.js",
-};
