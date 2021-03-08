09f7edfb5449f97ff69854d3d474d80fb839a799
Gene Culling <culling66@gmail.com>
2021-03-08 15:53:57 +1300

    Add barSpacing and barWidth

--- client_build/js/components/grades/grades-graph.js
+++ client_build/js/components/grades/grades-graph.js
@@ -1,13 +1,13 @@
 class GradesGraph extends HTMLElement {
   constructor() {
     super();
-    
+
     // Apply external styles to the shadow dom
     const linkElem = document.createElement('link');
     linkElem.setAttribute('rel', 'stylesheet');
     linkElem.setAttribute('href', 'css/main.css');
 
-    
+
     this.data = [
       {
         courseShortName: "COMP307",
@@ -36,7 +36,7 @@ class GradesGraph extends HTMLElement {
     ];
   }
 
-  
+
   connectedCallback() {
     this.innerHTML = `
     <style>
@@ -82,12 +82,12 @@ class GradesGraph extends HTMLElement {
     });
 
     const graphTheme = {
-      total:{
+      total: {
         fill: "#1E90FF",
         stroke: "#0E80EF",
         offset: 5
       },
-      impact:{
+      impact: {
         fill: "#87CEEB",
         stroke: "#77BEDB",
         offset: 0
@@ -106,9 +106,9 @@ class GradesGraph extends HTMLElement {
         height: 100,
         width: 120,
         margins: {
-          left:20,
+          left: 20,
           top: 0,
-          right: 0, 
+          right: 0,
           bottom: 0
         }
       },
@@ -117,7 +117,7 @@ class GradesGraph extends HTMLElement {
     // Make the SVG container
     const svg = target
       .append("svg")
-      .style("height",  svgConfig.margins.top + svgConfig.height + svgConfig.margins.bottom)
+      .style("height", svgConfig.margins.top + svgConfig.height + svgConfig.margins.bottom)
       .style("width", svgConfig.width);
 
     // -- Add the bars --
@@ -129,19 +129,21 @@ class GradesGraph extends HTMLElement {
       .append("rect")
       .attr("class", "bar field1")
       .attr("x", (d, i) => {
-        return this.getBarDrawingX(svgConfig, i, graphTheme.total.offset);
+        return this.getBarDrawingX(svgConfig, i, graphTheme.total.offset, this.data);
       })
       .attr("y", (d) => {
         return this.getBarDrawingHeight(svgConfig) - d.maximumPossibleGrade;
       })
-      .style("width", svgConfig.bar.width - svgConfig.bar.margins.left)
+      .style("width", (d) => {
+        return this.getBarWidth(svgConfig, data) - svgConfig.margins.left + "px";
+      })
       .style("height", (d) => {
         return d.maximumPossibleGrade;
       })
       .style("fill", graphTheme.total.fill)
       .style("stroke", graphTheme.total.stroke)
       .append("title")
-      .text((d)=>{
+      .text((d) => {
         return d.courseFullName;
       });
 
@@ -153,30 +155,32 @@ class GradesGraph extends HTMLElement {
       .append("rect")
       .attr("class", "bar field2")
       .attr("x", (d, i) => {
-        return this.getBarDrawingX(svgConfig, i, graphTheme.impact.offset);
+        return this.getBarDrawingX(svgConfig, i, graphTheme.impact.offset, this.data);
       })
       .attr("y", (d) => {
         return this.getBarDrawingHeight(svgConfig) - d.currentGradeTotal;
       })
-      .style("width", svgConfig.bar.width - svgConfig.bar.margins.left)
+      .style("width", () => {
+        return this.getBarWidth(svgConfig, data) - svgConfig.margins.left + "px";
+      })
       .style("height", (d) => {
         return d.currentGradeTotal;
       })
       .style("fill", graphTheme.impact.fill)
       .style("stroke", graphTheme.impact.stroke)
       .append("title")
-      .text((d)=>{
+      .text((d) => {
         return d.courseFullName;
       });
 
-    // // Add labels
+    // Add labels
     svg
       .selectAll("text")
       .data(data)
       .enter()
       .append("text")
       .attr("x", (d, i) => {
-        return this.getBarDrawingX(svgConfig, i, 0);
+        return this.getBarDrawingX(svgConfig, i, 0, this.data);
       })
       .attr("y", (d, i) => {
         return this.getBarDrawingHeight(svgConfig) + svgConfig.margins.bottom - 5;
@@ -187,44 +191,55 @@ class GradesGraph extends HTMLElement {
 
     // Add y axis
     const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, (d) => d.maximumPossibleGrade)])
-    .range(this.getRange(svgConfig, data));
+      .domain([0, d3.max(data, (d) => d.maximumPossibleGrade)])
+      .range(this.getRange(svgConfig, data));
     const yAxis = d3.axisLeft(yScale);
 
-      svg.append("g")
-      .attr("transform", `translate(${svgConfig.margins.left}, ${-1 * svgConfig.margins.bottom })`)
+    svg.append("g")
+      .attr("transform", `translate(${svgConfig.margins.left}, ${-1 * svgConfig.margins.bottom})`)
       .call(yAxis);
 
-      // Add Top and bottom box lines
-      // this.drawTopLine(svg, svgConfig, data);
-      this.drawBottomLine(svg, svgConfig, data);
-      
+    // Add Top and bottom box lines
+    // this.drawTopLine(svg, svgConfig, data);
+    this.drawBottomLine(svg, svgConfig, data);
+
   };
 
-  getBarDrawingHeight = (svgConfig)=>{
+  getBarWidth = (svgConfig, data) => {
+    const barWidth = `${this.getBarSpacing(svgConfig, data) - svgConfig.bar.margins.left}`;
+    return barWidth;
+  }
+
+  getBarSpacing = (svgConfig, data) => {
+    const barSpacing = `${(svgConfig.width) / data.length}`;
+    return barSpacing;
+  }
+
+  getBarDrawingHeight = (svgConfig) => {
     return svgConfig.height + svgConfig.margins.top;
   }
 
-  getBarDrawingX= (svgConfig, i, additionalOffset)=>{
-    return (i * svgConfig.bar.width) + svgConfig.margins.left + additionalOffset;
+  getBarDrawingX = (svgConfig, i, additionalOffset, data) => {
+    const barDrawingX = svgConfig.margins.left + additionalOffset + (i * (this.getBarSpacing(svgConfig, data)  )  );
+    return barDrawingX;
   }
 
-  getRange = (svgConfig, data)=>{
-    return [this.getBarDrawingHeight(svgConfig) + svgConfig.margins.bottom, this.getBarDrawingHeight(svgConfig) - (d3.max(data, (d) => d.maximumPossibleGrade) - svgConfig.margins.bottom) ];
+  getRange = (svgConfig, data) => {
+    return [this.getBarDrawingHeight(svgConfig) + svgConfig.margins.bottom, this.getBarDrawingHeight(svgConfig) - (d3.max(data, (d) => d.maximumPossibleGrade) - svgConfig.margins.bottom)];
   }
 
-  drawTopLine = (svg, svgConfig, data)=>{
+  drawTopLine = (svg, svgConfig, data) => {
     svg.append("line")
-    .style("stroke", "black")
-    .style("stroke-width", 1)
-    .attr("x1", 0)
-    .attr("y1", this.getRange(svgConfig, data)[1])
-    .attr("x2", svgConfig.width)
-    .attr("y2", this.getRange(svgConfig, data)[1])
-    .attr("transform", `translate(${svgConfig.margins.left}, ${-1 * svgConfig.margins.bottom })`)
+      .style("stroke", "black")
+      .style("stroke-width", 1)
+      .attr("x1", 0)
+      .attr("y1", this.getRange(svgConfig, data)[1])
+      .attr("x2", svgConfig.width)
+      .attr("y2", this.getRange(svgConfig, data)[1])
+      .attr("transform", `translate(${svgConfig.margins.left}, ${-1 * svgConfig.margins.bottom})`)
   }
 
-  drawBottomLine = (svg, svgConfig, data) =>{
+  drawBottomLine = (svg, svgConfig, data) => {
     svg.append("line")
       .style("stroke", "black")
       .style("stroke-width", 1)
@@ -232,7 +247,7 @@ class GradesGraph extends HTMLElement {
       .attr("y1", this.getRange(svgConfig, data)[0])
       .attr("x2", svgConfig.width)
       .attr("y2", this.getRange(svgConfig, data)[0])
-      .attr("transform", `translate(${svgConfig.margins.left}, ${-1 * svgConfig.margins.bottom })`)
+      .attr("transform", `translate(${svgConfig.margins.left}, ${-1 * svgConfig.margins.bottom})`)
   }
 
 }
